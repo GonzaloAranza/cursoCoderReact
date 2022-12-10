7dfe48ceb6cdf1eac75cbb704ff04480a67db0b2
GonzaloAranza <g.f.aranzamendi@gmail.com>
2022-12-10 17:20:20 -0300

    SegundaPreentrega+Aranzamendi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 
-import Counter from "./components/counter/Counter";
 import Header from "./components/header/Header";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
-
+import {BrowserRouter, Routes, Route, Router} from "react-router-dom"
+import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
 function App() {
+
+
   return (
-    <>
+  /*  <>
     <Header />
     <Counter />
-    <ItemListContainer props={'lista de articulos'}/>
-  
+    <ItemListContainer props={'lista de articulos'}/> 
     </> 
-    
+   */
+
+        <BrowserRouter>
+        
+          <Header />
+          
+          <Routes>
+
+              <Route path="/" element={<ItemListContainer/>}/>
+              
+              <Route path="/category/:categoryName" element={<ItemListContainer />} />
+
+              <Route path="/itemDetail/:id" element={<ItemDetailContainer  />}/>
+
+              <Route path="/cart" element={<h2>estoy en el carrito</h2>} />
+
+
+          </Routes>
+          
+        </BrowserRouter> 
     );
 }
 
